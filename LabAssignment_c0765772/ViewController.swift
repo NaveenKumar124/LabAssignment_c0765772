d3c52dc89843e43fcc31cc37004490485d2f487e
navimalhotra <navimalhotra124@gmail.com>
2020-01-14 16:57:48 -0500

    Initial Commit

---
+++ LabAssignment_c0765772/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LabAssignment_c0765772
+//
+//  Created by Navi Malhotra on 2020-01-14.
+//  Copyright © 2020 Naveen Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
